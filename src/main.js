bbf0dc513580a374380ae9a822f515d40cfa07cb
yuvmironov <146@armed.ru>
2018-05-18 19:00:42 +0300

    Стилизация + Форма для обратной связи

--- src/main.js
+++ src/main.js
@@ -4,15 +4,16 @@ var testBlock = document.getElementById('Test');
 var buttonTest = document.getElementById('TestActive');
 var buttonCheck = document.getElementById('TestCheck');
 var bodyContent = document.getElementById('body');
+var testForm = document.getElementById('TestForm');
 var count = 0;
 var randomMass = [];
 
 while (randomMass.length < 10) {
-	var temp = random(1,15);
+	var temp = random(1, 15);
 	chekMass(temp);
 }
 
-function chekMass (number) {
+function chekMass(number) {
 	for (var i = 0; i < randomMass.length; i++) {
 		if (randomMass[i] == number) {
 			return false
@@ -22,69 +23,100 @@ function chekMass (number) {
 }
 
 function random(min, max) {
-    var rand = min - 0.5 + Math.random() * (max - min + 1)
-    rand = Math.round(rand);
-    return rand;
+	var rand = min - 0.5 + Math.random() * (max - min + 1)
+	rand = Math.round(rand);
+	return rand;
 }
 
-function getJson () {
-	$.getJSON('q.json', function(data) {
+function getJson() {
+	$.getJSON('q.json', function (data) {
 		count = createTest(data);
 	})
 }
 
-function createTest (data) {
+function createTest(data) {
 	var questionsCount = 0;
-	testBlock.innerHTML = '';
+	testBlock.innerHTML = '<h2 class="Header">Ответьте на 10 вопросов за<br><span id="timer" long="10:00">10:00</span></h2>';
 	for (var j = 0; j < randomMass.length; j++) {
 		questionsCount++;
 		var questions = data[randomMass[j]];
-		console.log("questions", questions);
-		var question = '<p class="Test-Question">'+questions.q+'</p><div class="Test-LabelWrap">';
+		var question = '<p class="Test-Question">' + questions.q + '</p><div class="Test-LabelWrap">';
 		for (var i = 0; i < questions.a.length; i++) {
 			if (i == questions.ansver) {
-				question += '<label class="Test-Label">';
-				question += '<input type="radio" name="q'+randomMass[j]+'" value="true">';
-				question += questions.a[i] + '</label>';
+				question += '<div class="radioButton">'
+				question += '<input type="radio" id="i' + randomMass[j] + 'q'+ i +'" name="q' + randomMass[j] + '" value="true">';
+				question += '<label for="i' + randomMass[j] + 'q'+ i +'" class="radio-label Test-Label" >';
+				question += questions.a[i] + '</label></div>';
 			} else {
-				question += '<label class="Test-Label">';
-				question += '<input type="radio" name="q'+randomMass[j]+'" value="false">';
-				question += questions.a[i] + '</label>';
+				question += '<div class="radioButton">'
+				question += '<input type="radio" id="i' + randomMass[j] + 'q'+ i +'" name="q' + randomMass[j] + '" value="false">';
+				question += '<label for="i' + randomMass[j] + 'q'+ i +'" class="radio-label Test-Label" >';
+				question += questions.a[i] + '</label></div>';
 			}
 		}
-		question += '</div><hr/>';
+		question += '</div>';
 		testBlock.innerHTML += question;
 	}
-	var button = '<a href="#" class="TestCheck Button Button_Color_Red Button_Size_Lg" onclick="checkTest(); return false;">';
+	var button = '<a href="#" class="Test-Check Button Button_Color_Red Button_Size_Lg" onclick="checkTest(); return false;">';
 	button += '<span class="Button-Text Button-Text_Color_White Button-Text_Uppercase">проверить<span></a>';
 	testBlock.innerHTML += button;
 	return questionsCount;
 }
-function checkTest () {
+
+function checkTest() {
 	var inputMass = document.getElementsByTagName('input');
 	var itogo = 0;
 	for (var i = 0; i < inputMass.length; i++) {
 		if (inputMass[i].checked) {
 			if (inputMass[i].value == 'true') {
-				itogo++; 	
+				itogo++;
 			}
 		}
 	}
 	console.log('itogo', itogo);
 	console.log('count ', count);
-	if (itogo < (count-1)) {
-		testBlock.innerHTML = '<h1>Вы не смогли пройти тест, советуем еще раз просмотреть видео и пройти тест заново. Количество правильных ответов '+itogo+' из '+count+' </h1>';
+	if (itogo < (count - 1)) {
+		testBlock.innerHTML = '<h1 class="Test-Answer">Вы не смогли пройти тест, советуем еще раз просмотреть видео и пройти тест заново.<br> Количество правильных ответов<span class="ErrorAnswer">&nbsp;' + itogo + '</span> из ' + count + ' </h1>';
 		videoHeader.classList.remove('Hide');
 		videoBlock.classList.remove('Hide');
+		buttonTest.classList.remove('Hide');
+		//timer.classList.add('Hide');
 	} else {
-		bodyContent.innerHTML = '<h1>Поздравляем, вы успешно прошли тест, данное окно можно закрыть. Количество правильных ответов '+itogo+' из '+count+' </h1>';
+		bodyContent.innerHTML = '<h1 class="Test-Answer">Поздравляем, вы успешно прошли тест, данное окно можно закрыть.<br> Количество правильных ответов <span class="GoodAnswer">' + itogo + '</span> из ' + count + ' </h1>';
+		testForm.classList.remove('Hide')
 	}
 }
 
-buttonTest.addEventListener('click', function(event){
+buttonTest.addEventListener('click', function (event) {
 	event.preventDefault();
 	videoHeader.classList.add('Hide');
 	videoBlock.classList.add('Hide');
 	testBlock.classList.remove('Hide');
+	buttonTest.classList.add('Hide');
+	//timer.classList.remove('Hide');
+	var t = setInterval(function () {
+		function f(x) {
+			return (x / 100).toFixed(2).substr(2)
+		}
+		var o = document.getElementById('timer'),
+			w = 60,
+			y = o.innerHTML.split(':'),
+			v = y[0] * w + (y[1] - 1),
+			s = v % w,
+			m = (v - s) / w;
+		if (s < 0)
+			var v = o.getAttribute('long').split(':'),
+				m = v[0],
+				s = v[1];
+		o.innerHTML = [f(m), f(s)].join(':');
+		if (v == 0) {
+			clearInterval(t);
+			testBlock.innerHTML = '<h1 class="Test-Answer">Ваше время на прохождение теста истекло, просмотрите видео заново и пройдите тест</h1>';
+			videoHeader.classList.remove('Hide');
+			videoBlock.classList.remove('Hide');
+			buttonTest.classList.remove('Hide');
+			//timer.classList.add('Hide');
+		}
+	}, 1000);
 	count = getJson();
 });
